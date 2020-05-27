71f69e28ba13b155b93a7a63046c57db35d26aaa
Andrelino Silva <andrelinocom@gmail.com>
2020-05-26 22:04:51 -0300

    Add validação de campos e formatação de erros

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,15 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+import Tooltip from '../ToolTip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #222f3e;
   border-radius: 10px;
   border: 2px solid #8395a7;
@@ -15,10 +24,31 @@ export const Container = styled.div`
     margin-top: 8px;
   }
 
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
+
+
   input {
     flex: 1;
     border: 0;
-    background: transparent;
+    background: none;
     color: #f4ede8;
 
     &::placeholder {
@@ -30,3 +60,23 @@ export const Container = styled.div`
     margin-right: 16px;
   }
 `;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 17px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+
+
+`
